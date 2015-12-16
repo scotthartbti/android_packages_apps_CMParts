9908540768215f8fd053047414debf2be742063b
eyosen <abittin@gmail.com>
2015-12-16 01:43:16 -0500

    Add Screen Record to the Power Menu [2/2] PureNexus Edits: *adapted to work with our custom powermenu option
    Change-Id: If851deb354f1ad787b3f07ef720a9b30bb768851
    Signed-off-by: Scott <scott@ubuntu.com>

--- src/org/cyanogenmod/cmparts/input/PowerMenuActions.java
+++ src/org/cyanogenmod/cmparts/input/PowerMenuActions.java
@@ -56,6 +56,7 @@ public class PowerMenuActions extends SettingsPreferenceFragment {
     private CheckBoxPreference mRebootPref;
     private CheckBoxPreference mScreenshotPref;
     private CheckBoxPreference mTorchPref;
+    private CheckBoxPreference mScreenRecordPref;
     private CheckBoxPreference mAirplanePref;
     private CheckBoxPreference mUsersPref;
     private CheckBoxPreference mSettingsPref;
@@ -92,6 +93,8 @@ public class PowerMenuActions extends SettingsPreferenceFragment {
                 mRebootPref = (CheckBoxPreference) findPreference(GLOBAL_ACTION_KEY_REBOOT);
             } else if (action.equals(GLOBAL_ACTION_KEY_SCREENSHOT)) {
                 mScreenshotPref = (CheckBoxPreference) findPreference(GLOBAL_ACTION_KEY_SCREENSHOT);
+	    } else if (action.equals(GLOBAL_ACTION_KEY_SCREENRECORD)) {
+                mScreenRecordPref = (CheckBoxPreference) findPreference(GLOBAL_ACTION_KEY_SCREENRECORD);
             } else if (action.equals(GLOBAL_ACTION_KEY_TORCH)) {
                 mTorchPref = (CheckBoxPreference) findPreference(GLOBAL_ACTION_KEY_TORCH);
             } else if (action.equals(GLOBAL_ACTION_KEY_AIRPLANE)) {
@@ -126,6 +129,10 @@ public class PowerMenuActions extends SettingsPreferenceFragment {
 
         if (mScreenshotPref != null) {
             mScreenshotPref.setChecked(settingsArrayContains(GLOBAL_ACTION_KEY_SCREENSHOT));
+        }
+
+	if (mScreenRecordPref != null) {
+            mScreenRecordPref.setChecked(settingsArrayContains(GLOBAL_ACTION_KEY_SCREENRECORD));
         }
 
         if (mTorchPref != null) {
@@ -198,6 +205,10 @@ public class PowerMenuActions extends SettingsPreferenceFragment {
         } else if (preference == mScreenshotPref) {
             value = mScreenshotPref.isChecked();
             updateUserConfig(value, GLOBAL_ACTION_KEY_SCREENSHOT);
+
+	} else if (preference == mScreenRecordPref) {
+            value = mScreenRecordPref.isChecked();
+            updateUserConfig(value, GLOBAL_ACTION_KEY_SCREENRECORD);
 
         } else if (preference == mTorchPref) {
             value = mTorchPref.isChecked();
