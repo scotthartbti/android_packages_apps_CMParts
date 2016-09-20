cfe7bf02761e9fca177a37b7f4d3ad18586fb6b2
Scott <scott@ubuntu.com>
2016-09-20 11:44:31 -0400

    Revert "Remove bugreport from power menu options since we dont use it"
    This reverts commit a0c014740ca2ba255c46842b772c71789bf9c375.

--- src/org/cyanogenmod/cmparts/input/PowerMenuActions.java
+++ src/org/cyanogenmod/cmparts/input/PowerMenuActions.java
@@ -40,6 +40,7 @@ import cyanogenmod.providers.CMSettings;
 
 import static com.android.internal.util.cm.PowerMenuConstants.GLOBAL_ACTION_KEY_AIRPLANE;
 import static com.android.internal.util.cm.PowerMenuConstants.GLOBAL_ACTION_KEY_ASSIST;
+import static com.android.internal.util.cm.PowerMenuConstants.GLOBAL_ACTION_KEY_BUGREPORT;
 import static com.android.internal.util.cm.PowerMenuConstants.GLOBAL_ACTION_KEY_LOCKDOWN;
 import static com.android.internal.util.cm.PowerMenuConstants.GLOBAL_ACTION_KEY_REBOOT;
 import static com.android.internal.util.cm.PowerMenuConstants.GLOBAL_ACTION_KEY_SCREENSHOT;
@@ -57,6 +58,7 @@ public class PowerMenuActions extends SettingsPreferenceFragment {
     private CheckBoxPreference mUsersPref;
     private CheckBoxPreference mSettingsPref;
     private CheckBoxPreference mLockdownPref;
+    private CheckBoxPreference mBugReportPref;
     private CheckBoxPreference mSilentPref;
     private CheckBoxPreference mVoiceAssistPref;
     private CheckBoxPreference mAssistPref;
@@ -96,6 +98,8 @@ public class PowerMenuActions extends SettingsPreferenceFragment {
                 mSettingsPref = (CheckBoxPreference) findPreference(GLOBAL_ACTION_KEY_SETTINGS);
             } else if (action.equals(GLOBAL_ACTION_KEY_LOCKDOWN)) {
                 mLockdownPref = (CheckBoxPreference) findPreference(GLOBAL_ACTION_KEY_LOCKDOWN);
+            } else if (action.equals(GLOBAL_ACTION_KEY_BUGREPORT)) {
+                mBugReportPref = (CheckBoxPreference) findPreference(GLOBAL_ACTION_KEY_BUGREPORT);
             } else if (action.equals(GLOBAL_ACTION_KEY_SILENT)) {
                 mSilentPref = (CheckBoxPreference) findPreference(GLOBAL_ACTION_KEY_SILENT);
             } else if (action.equals(GLOBAL_ACTION_KEY_VOICEASSIST)) {
@@ -145,6 +149,10 @@ public class PowerMenuActions extends SettingsPreferenceFragment {
             mLockdownPref.setChecked(settingsArrayContains(GLOBAL_ACTION_KEY_LOCKDOWN));
         }
 
+        if (mBugReportPref != null) {
+            mBugReportPref.setChecked(settingsArrayContains(GLOBAL_ACTION_KEY_BUGREPORT));
+        }
+
         if (mSilentPref != null) {
             mSilentPref.setChecked(settingsArrayContains(GLOBAL_ACTION_KEY_SILENT));
         }
@@ -157,11 +165,13 @@ public class PowerMenuActions extends SettingsPreferenceFragment {
             mAssistPref.setChecked(settingsArrayContains(GLOBAL_ACTION_KEY_ASSIST));
         }
 
+        updatePreferences();
     }
 
     @Override
     public void onResume() {
         super.onResume();
+        updatePreferences();
     }
 
     @Override
@@ -191,6 +201,10 @@ public class PowerMenuActions extends SettingsPreferenceFragment {
         } else if (preference == mLockdownPref) {
             value = mLockdownPref.isChecked();
             updateUserConfig(value, GLOBAL_ACTION_KEY_LOCKDOWN);
+
+        } else if (preference == mBugReportPref) {
+            value = mBugReportPref.isChecked();
+            updateUserConfig(value, GLOBAL_ACTION_KEY_BUGREPORT);
 
         } else if (preference == mSilentPref) {
             value = mSilentPref.isChecked();
@@ -232,6 +246,20 @@ public class PowerMenuActions extends SettingsPreferenceFragment {
             }
         }
         saveUserConfig();
+    }
+
+    private void updatePreferences() {
+        boolean bugreport = Settings.Secure.getInt(getContentResolver(),
+                Settings.Secure.BUGREPORT_IN_POWER_MENU, 0) != 0;
+
+        if (mBugReportPref != null) {
+            mBugReportPref.setEnabled(bugreport);
+            if (bugreport) {
+                mBugReportPref.setSummary(null);
+            } else {
+                mBugReportPref.setSummary(R.string.power_menu_bug_report_disabled);
+            }
+        }
     }
 
     private void getUserConfig() {
